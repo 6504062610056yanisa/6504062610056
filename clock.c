9045a1e08324cee6e0b8a5aa365dcbd1e1878fe6
6504062610056yanisa <s6504062610056@email.kmutnb.ac.th>
2025-12-23 13:11:17 +0700

    Update clock.c
    version + เวลาเพิ่มและบอกเวลาข้ามวัน
    ตัวแปร next คือตัวแปร + เวลานาทีที่ผู้ใช้ป้อน
    ตัวแปร addHour คือตัวแปรที่ใช้เก็บจำนวนชั่วโมงที่เพิ่มขึ้นจากการบวกนาที

--- clock.c
+++ clock.c
@@ -2,10 +2,23 @@
 
 int main()
 {
-    int h, m, s;
+    int h, m, s, next;
+    int addHour;
+
     scanf("%d:%d:%d", &h, &m, &s);
+    scanf("%d", &next);
 
-    printf("hour:%d\nminute:%d\nsecond:%d", h, m, s);
+    m += next;
+    
+    addHour = m / 60;
+    m = m % 60;
+    h = h + addHour;
 
+        printf("%d:%d:%d\n", h, m, s);
+        printf("to next day : %d hour %d minute %d second\n",
+               23 - h,
+               59 - m,
+               60 - s);
+  
     return 0;
 }
